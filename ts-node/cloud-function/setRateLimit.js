715035bf7341a73d3131e4ff6350171ff303676f
Laam Wong <laam@Laams-iMac.local>
2022-07-05 07:24:47 +0800

    api call created

---
+++ ts-node/cloud-function/setRateLimit.js
@@ -0,0 +1,5 @@
+Moralis.settings.setAPIRateLimit({
+  anonymous: 1000,
+  authenticated: 2000,
+  windowMs: 60000,
+});
---
+++ ts-node/src/getNfts.ts
@@ -0,0 +1,41 @@
+import * as fs from "fs";
+import { Moralis as MoralisDef } from "moralis/types/node";
+import * as MoralisImport from "moralis/node";
+import { config } from "dotenv";
+const Moralis = MoralisImport as unknown as MoralisDef;
+
+config({ path: "../.env" });
+
+const contractAddress = "0x23581767a106ae21c074b2276D25e5C3e136a68b"; //Moonbirds
+
+const getAllOwners = async () => {
+  let cursor: string = "";
+  let owner = {};
+  let history = {};
+  let res;
+  let accountedToken = [];
+
+  for (let i = 0; i < 5; i++) {
+    const response = await Moralis.Web3API.token.getContractNFTTransfers({
+      address: contractAddress,
+      chain: "eth",
+      limit: 3,
+      cursor: cursor,
+    });
+
+    console.log(
+      `Got page ${response.page} of ${Math.ceil(
+        response.total / response.page_size
+      )}, ${response.total} in totals`
+    );
+    cursor = response.cursor;
+  }
+};
+
+(async () => {
+  await Moralis.start({
+    appId: process.env.APP_ID,
+    serverUrl: process.env.SERVER_URL,
+  });
+  await getAllOwners();
+})();
