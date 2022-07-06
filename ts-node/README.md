188527db453be46cceeb255f017b05256f53b975
Laam Wong <laam@Laams-iMac.local>
2022-07-06 09:23:44 +0800

    added manual fetch feature

---
+++ ts-node/README.md
@@ -0,0 +1,8 @@
+# Fetch NFTs Manually
+
+Fetch NFTs data manually if required in case of abusing the api servers
+
+```bash
+$ cd src/
+$ ts-node getNfts.ts
+```
--- ts-node/src/getNfts.ts
+++ ts-node/src/getNfts.ts
@@ -2,36 +2,98 @@ import * as fs from "fs";
 import { Moralis as MoralisDef } from "moralis/types/node";
 import * as MoralisImport from "moralis/node";
 import { config } from "dotenv";
-const Moralis = MoralisImport as unknown as MoralisDef;
-
 config({ path: "../.env" });
+
+interface OwnersDetailsTypes {
+  address: string;
+  amount: number;
+  tokenId: string[];
+  price: number[];
+  date: string[];
+}
+interface HistoryDetailsTypes {
+  to: string;
+  from: string;
+  tokenId: string[];
+  price: number[];
+  date: string[];
+}
+interface OwnersTypes {
+  [index: string]: OwnersDetailsTypes;
+}
+interface HistoryTypes {
+  [index: string]: HistoryDetailsTypes[];
+}
+type AccountTokenTypes = Array<string> & { [index: string]: string };
+// type OwnersTypes = Array<string> & {
+//   [index: string]: OwnersDetailsTypes;
+// };
+
+const Moralis = MoralisImport as unknown as MoralisDef;
 
 const contractAddress = "0x23581767a106ae21c074b2276D25e5C3e136a68b"; //Moonbirds
 
 const getAllOwners = async () => {
-  let cursor: string = "";
-  let owner = {};
-  let history = {};
-  let res;
-  let accountedToken = [];
+  let owners = {} as OwnersTypes;
+  let history = {} as HistoryTypes;
+  let accountedToken = [] as unknown as AccountTokenTypes;
 
   const response = await Moralis.Web3API.token.getContractNFTTransfers({
     address: contractAddress,
     chain: "eth",
-    limit: 10,
-    cursor: cursor,
+    limit: 100,
   });
 
-  for (const transfer of response.result) {
-  }
+  if (response.result) {
+    for (const transfer of response.result) {
+      if (
+        !owners[transfer.to_address] &&
+        !accountedToken.includes(transfer.token_id)
+      ) {
+        owners[transfer.to_address] = {
+          address: transfer.to_address,
+          amount: Number(transfer.amount),
+          tokenId: [transfer.token_id],
+          price: [Number(transfer.value)],
+          date: [transfer.block_timestamp],
+        };
 
+        accountedToken.push(transfer.token_id);
+      } else if (!accountedToken.includes(transfer.token_id)) {
+        owners[transfer.to_address].amount++;
+        owners[transfer.to_address].tokenId.push(transfer.token_id);
+        owners[transfer.to_address].price.push(Number(transfer.value));
+        owners[transfer.to_address].date.push(transfer.block_timestamp);
+        accountedToken.push(transfer.token_id);
+      }
+
+      if (!history[transfer.to_address] && transfer.from_address) {
+        history[transfer.to_address] = [
+          {
+            to: transfer.to_address,
+            from: transfer.from_address,
+            tokenId: [transfer.token_id],
+            price: [Number(transfer.value)],
+            date: [transfer.block_timestamp],
+          },
+        ];
+      } else if (transfer.from_address) {
+        history[transfer.to_address].push({
+          to: transfer.to_address,
+          from: transfer.from_address,
+          tokenId: [transfer.token_id],
+          price: [Number(transfer.value)],
+          date: [transfer.block_timestamp],
+        });
+      }
+    }
+  }
+  fs.writeFileSync("./owners.json", JSON.stringify(owners, null, 2));
+  fs.writeFileSync("./history.json", JSON.stringify(history, null, 2));
+  // console.log(owners);
+  // console.log(accountedToken.length);
   // console.log(response);
 
-  console.log(
-    `Got page ${response.page} of ${Math.ceil(
-      response.total / response.page_size
-    )}, ${response.total} in totals`
-  );
   // cursor = response.cursor;
 };
 
@@ -41,4 +103,4 @@ const getAllOwners = async () => {
     serverUrl: process.env.SERVER_URL,
   });
   await getAllOwners();
-})();
+})().catch((error) => console.log(error));
